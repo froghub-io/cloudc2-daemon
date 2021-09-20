35e6e118d0151e4e1c083dffc63c1781f2bf5ccc
LazyPig <froghub@163.com>
2021-09-20 14:54:48 +0800

    脚本更新

--- stop.sh
+++ stop.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 export PATH="/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin"
-pid=$(ps x | grep "cloudc2-daemon daemon" | grep -v grep | awk '{print $1}')
+pid=$(ps x | grep "cloudc2-daemon" | grep -v grep | awk '{print $1}')
 if [ $pid ]; then
   kill -9 $pid
 fi
